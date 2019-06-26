cea0891344a6a37e522f4725a8ed14af2401e51f
emily <mle4d@me.com>
2019-06-25 17:16:48 -0700

    rename works

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const { join } = require('path');
-fs.readFile(join(__dirname, './files/2.txt'), {encoding: 'utf8' }, (err, data) => {
+fs.readFile(join(__dirname, './files/2.txt'), { encoding: 'utf8' }, (err, data) => {
   if(err) throw err;
   console.log(data);
 });
 
-fs.rename('./files/2.txt')
+fs.rename('./files/2.txt', './files/unicorns.txt', err => {
+  if(err) throw (err);
+
+});
+
 // module.exports = {
 //   fs
-// }
